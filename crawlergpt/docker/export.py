46b7dc6811b773e36ca60f61c4e6cccb19fed933
Nfsaavedra <nuno.saavedra@tecnico.ulisboa.pt>
2023-06-21 16:09:54 +0100

    extract and apply docker diff

--- crawlergpt/docker/export.py
+++ crawlergpt/docker/export.py
@@ -1,12 +1,15 @@
 import os
+import time
 import hashlib
 import shutil
 import uuid
 import tempfile
 import json
+import pickle
 import docker
 import tarfile
 from dataclasses import dataclass
+from typing import Dict
 
 from docker.models.containers import Container
 from docker.models.images import Image
@@ -62,6 +65,7 @@ def extract_last_layer(container_id: str, layer_path: str) -> Layer:
 
     return Layer(layer, os.path.join(layer_path, layer))
 
+
 def add_new_layer(image_name: str, layer: Layer, new_image_name: str = None):
     client = docker.from_env(timeout=1200)
     image: Image = client.images.get(image_name)
@@ -115,3 +119,106 @@ def add_new_layer(image_name: str, layer: Layer, new_image_name: str = None):
             os.remove(tar_path)
         if os.path.exists(final_tar):
             os.remove(final_tar)
+
+@dataclass
+class DiffNode:
+    children: Dict[str, 'DiffNode']
+    kind: int
+    path: str
+    full_path: str
+
+    @property
+    def is_file(self) -> bool:
+        return len(self.children) == 0
+
+
+def extract_diff(container_id: str, diff_file_path: str):
+    save_path = os.path.join(tempfile.gettempdir(), str(uuid.uuid4()))
+    client = docker.from_env(timeout=600)
+    container: Container = client.containers.get(container_id)
+    
+    # Kinds: 0 -> Changed, 1 -> Created, 2 -> Deleted
+    diff = container.diff()
+    parent_node = DiffNode({}, -1, '/', '/')
+
+    for change in diff:
+        # The index removes the empty string from the beggining (/path...)
+        path = change['Path'].split(os.sep)[1:]
+        current_node = parent_node
+
+        for p in path:
+            if p not in current_node.children:
+                current_node.children[p] = DiffNode({}, -1, p, '')
+            current_node = current_node.children[p]
+
+        current_node.kind = change['Kind']
+        current_node.full_path = change['Path']
+    
+    if not os.path.exists(save_path):
+        os.makedirs(save_path)
+    
+    def handle_node(node: DiffNode):
+        for _, child in node.children.items(): 
+            if child.kind == 2:
+                continue
+            elif child.is_file:
+                file_path = os.path.join(save_path, child.full_path[1:])
+                with open(f'{file_path}.tar', 'wb') as f:
+                    bits, _ = container.get_archive(child.full_path)
+                    for chunk in bits:
+                        f.write(chunk)
+                
+                with tarfile.open(f'{file_path}.tar', 'r') as f:
+                    f.extractall(os.path.dirname(file_path))
+
+                os.remove(f'{file_path}.tar')
+            else:
+                os.makedirs(os.path.join(save_path, child.full_path[1:]))
+            handle_node(child)
+
+    handle_node(parent_node)
+
+    with open(os.path.join(save_path, 'diff.pkl'), 'wb') as f:
+        pickle.dump(parent_node, f)
+    with tarfile.open(diff_file_path, 'w:gz') as tar_gz:
+        tar_gz.add(save_path, arcname="diff")
+    
+    shutil.rmtree(save_path)
+
+
+def apply_diff(container_id: str, diff_file_path: str):
+    client = docker.from_env(timeout=600)
+    container: Container = client.containers.get(container_id)
+    diff_path = os.path.join(tempfile.gettempdir(), str(uuid.uuid4()))
+
+    with tarfile.open(diff_file_path, 'r:gz') as tar_gz:
+        tar_gz.extractall(diff_path)
+    
+    with open(os.path.join(diff_path, 'diff', 'diff.pkl'), 'rb') as f:
+        parent_node: DiffNode = pickle.load(f)
+
+    def handle_removes(node: DiffNode):
+        for _, child in node.children.items():
+            handle_removes(child)
+
+        if not node.is_file and node.kind == 2:
+            container.exec_run(f'rmdir {node.full_path}')
+        elif node.is_file and node.kind == 2:
+            container.exec_run(f'rm {node.full_path}')
+
+    for file in os.listdir(os.path.join(diff_path, 'diff')):
+        if file != 'diff.pkl':
+            random_path = os.path.join(tempfile.gettempdir(), str(uuid.uuid4()))
+            with tarfile.open(random_path, "w") as tar:
+                tar.add(os.path.join(diff_path, 'diff', file), arcname=file)
+            with open(random_path, 'rb') as tar:
+                container.put_archive('/', tar.read())
+            os.remove(random_path)
+
+    handle_removes(parent_node)
+    shutil.rmtree(diff_path)
+
+
+extract_diff('8b74bae099c2', 'diff.tar.gz')
+apply_diff('f2dc9df3a875', 'diff.tar.gz')
+os.remove("diff.tar.gz")
