2cc27073fabb8fcbb2776fac21ac236a3c573954
André Silva <andre15andre@hotmail.com>
2023-07-02 15:57:25 +0200

    store more commit info (#51)
    store more commit info. closes #50

--- test/integration_tests/test_collect_bugs.py
+++ test/integration_tests/test_collect_bugs.py
@@ -87,6 +87,15 @@ class TestCollectBugs:
             assert len(lines) == 2
             data = json.loads(lines[0])
             assert data["commit_hash"] == "7e11161b4983f8ff9fd056fa465c8cabaa8a7f80"
+            assert data["commit_message"] == "Fix subtract. Fixes #1\n"
+            assert data["commit_timestamp"] == "2023-06-16T14:16:27Z"
+            assert (
+                data["previous_commit_hash"]
+                == "5871e6f8f072b4b3e07d4753c55c6c6302419b1e"
+            )
+            assert data["previous_commit_message"] == "Add subtract feature\n"
+            assert data["previous_commit_timestamp"] == "2023-06-10T15:22:49Z"
+            assert data["time_to_patch"] == "5 days, 22:53:38"
             assert data["strategy"] == "FAIL_PASS"
             assert len(data["actions_runs"]) == 3
             assert len(data["actions_runs"][0][0]["tests"]) == 2
@@ -104,6 +113,15 @@ class TestCollectBugs:
 
             data = json.loads(lines[1])
             assert data["commit_hash"] == "ef34d133079591972a5ce9442cbcc7603003d938"
+            assert data["commit_message"] == "Fix sum\n"
+            assert data["commit_timestamp"] == "2023-06-05T13:19:21Z"
+            assert (
+                data["previous_commit_hash"]
+                == "cad6744850817b708400736678d2601cbd1c1dd6"
+            )
+            assert data["previous_commit_message"] == "Add gitignore\n"
+            assert data["previous_commit_timestamp"] == "2023-06-05T13:19:12Z"
+            assert data["time_to_patch"] == "0:00:09"
             assert data["strategy"] == "PASS_PASS"
             assert len(data["actions_runs"]) == 3
             assert len(data["actions_runs"][1][0]["tests"]) == 1
@@ -112,7 +130,6 @@ class TestCollectBugs:
                 data["actions_runs"][1][0]["tests"][0]["results"][0]["result"]
                 == "Failure"
             )
-            assert data["commit_timestamp"] == "2023-06-05T13:19:21Z"
 
     @pytest.mark.dependency()
     def test_crawlergpt_pytest_test_repo(self):
@@ -129,6 +146,15 @@ class TestCollectBugs:
             assert len(lines) == 1
             data = json.loads(lines[0])
             assert data["commit_hash"] == "0e1907f75fcd3936b6d64292bc278250f2ee9ca3"
+            assert data["commit_message"] == "fix sum\n"
+            assert data["commit_timestamp"] == "2023-06-09T20:06:31Z"
+            assert (
+                data["previous_commit_hash"]
+                == "fd90a022e97988819f93abdc8828dd75e5f50776"
+            )
+            assert data["previous_commit_message"] == "initial implementation\n"
+            assert data["previous_commit_timestamp"] == "2023-06-09T20:05:56Z"
+            assert data["time_to_patch"] == "0:00:35"
             assert data["strategy"] == "PASS_PASS"
             assert len(data["actions_runs"]) == 3
             # assert that number of total tests before == 6 and all pass
@@ -195,7 +221,6 @@ class TestCollectBugs:
                     ]
                 ]
             )
-            assert data["commit_timestamp"] == "2023-06-09T20:06:31Z"
 
     @pytest.mark.dependency()
     def test_crawlergpt_gradle_test_repo(self):
@@ -212,6 +237,15 @@ class TestCollectBugs:
             assert len(lines) == 1
             data = json.loads(lines[0])
             assert data["commit_hash"] == "2289b33a322f01b95405905c53770a63fa21b8bf"
+            assert data["commit_message"] == "fix sum\n"
+            assert data["commit_timestamp"] == "2023-06-10T15:07:36Z"
+            assert (
+                data["previous_commit_hash"]
+                == "cab2445ecf7788bed39802d716ae095fc499cafa"
+            )
+            assert data["previous_commit_message"] == "initial implementation\n"
+            assert data["previous_commit_timestamp"] == "2023-06-10T15:07:10Z"
+            assert data["time_to_patch"] == "0:00:26"
             assert len(data["actions_runs"]) == 3
             # assert that number of total tests before == 1 and it passes
             assert len(data["actions_runs"][0][0]["tests"]) == 1
@@ -277,7 +311,6 @@ class TestCollectBugs:
                     ]
                 ]
             )
-            assert data["commit_timestamp"] == "2023-06-10T15:07:36Z"
 
     @pytest.mark.dependency()
     def test_crawlergpt_unittest_test_repo(self):
@@ -294,6 +327,15 @@ class TestCollectBugs:
             assert len(lines) == 1
             data = json.loads(lines[0])
             assert data["commit_hash"] == "d3d7a607e3a8abc330f8fd69f677284a9afaf650"
+            assert data["commit_timestamp"] == "2023-06-20T14:54:30Z"
+            assert data["commit_message"] == "fix sum\n"
+            assert (
+                data["previous_commit_hash"]
+                == "73a8a00fc8bbbe0af1351b4e97682138a32981b2"
+            )
+            assert data["previous_commit_message"] == "initial implementation\n"
+            assert data["previous_commit_timestamp"] == "2023-06-20T14:54:25Z"
+            assert data["time_to_patch"] == "0:00:05"
             assert data["strategy"] == "PASS_PASS"
             assert len(data["actions_runs"]) == 3
             # assert that number of total tests before == 2 and all pass
@@ -360,7 +402,6 @@ class TestCollectBugs:
                     ]
                 ]
             )
-            assert data["commit_timestamp"] == "2023-06-20T14:54:30Z"
 
     @pytest.mark.dependency(
         depends=[
