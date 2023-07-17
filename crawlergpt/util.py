96d831e1d9792c8f1bdd146199b1385684750053
André Silva <andre15andre@hotmail.com>
2023-07-17 21:06:48 +0200

    split non code changes from code changes (#93)
    * split non code changes
    
    * Update pyyaml version
    
    ---------
    
    Co-authored-by: Nfsaavedra <nuno.saavedra@tecnico.ulisboa.pt>

--- crawlergpt/util.py
+++ crawlergpt/util.py
@@ -6,8 +6,10 @@ import shutil
 import time
 import pygit2
 import subprocess
-from typing import Optional
+from unidiff import PatchSet
+from typing import Optional, List
 from crawlergpt.actions.actions import GitHubActions
+from enum import Enum
 
 
 def delete_repo_clone(repo_clone: pygit2.Repository):
@@ -76,3 +78,37 @@ def get_default_github_actions(
                 continue
     finally:
         repo_clone.reset(first_commit.oid, pygit2.GIT_RESET_HARD)
+
+
+class FileType(Enum):
+    SOURCE = 0
+    TESTS = 1
+    NON_SOURCE = 2
+
+
+def get_file_extension(file_path: str) -> str:
+    return file_path.split(".")[-1] if "." in file_path else file_path.split(os.sep)[-1]
+
+
+def get_patch_file_extensions(patch: PatchSet) -> List[str]:
+    return list(
+        {get_file_extension(p.source_file) for p in patch}.union(
+            {get_file_extension(p.target_file) for p in patch}
+        )
+    )
+
+
+def get_file_type(language: str, file_path: str) -> FileType:
+    language_extensions = {
+        "java": {"java"},
+        "python": {"py"},
+    }
+    test_keywords = {"test", "tests"}
+
+    if any([keyword in file_path.split(os.sep) for keyword in test_keywords]):
+        return FileType.TESTS
+
+    if get_file_extension(file_path) in language_extensions[language]:
+        return FileType.SOURCE
+    else:
+        return FileType.NON_SOURCE
