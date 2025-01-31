a8bb555cd57d929d1a7c45cdca6e22a99230d7ef
André Silva <andre15andre@hotmail.com>
2025-01-31 17:36:22 +0100

    feat: support npm+vitest (#238)

---
+++ gitbugactions/actions/javascript/npm/npm_vitest_workflow.py
@@ -0,0 +1,30 @@
+from typing import List
+from junitparser import TestCase
+from pathlib import Path
+
+from gitbugactions.actions.javascript.npm.npm_workflow import NpmWorkflow
+from gitbugactions.actions.multi.junitxmlparser import JUnitXMLParser
+
+
+class NpmVitestWorkflow(NpmWorkflow):
+
+    @classmethod
+    def is_npm_test_command(cls, command: str) -> bool:
+        return "vitest" in command.lower()
+
+    def instrument_test_steps(self):
+        if "jobs" in self.doc:
+            for _, job in self.doc["jobs"].items():
+                if "steps" in job:
+                    for step in job["steps"]:
+                        if "run" in step and self._is_test_command(step["run"]):
+                            step["run"] = (
+                                f"{step['run']} -- --reporter=junit --outputFile=junit.xml"
+                            )
+
+    def get_test_results(self, repo_path) -> List[TestCase]:
+        parser = JUnitXMLParser()
+        return parser.get_test_results(str(Path(repo_path, "junit.xml")))
+
+    def get_build_tool(self) -> str:
+        return "npm-vitest"
